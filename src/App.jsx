1a467ed2327f92ebd55331a3add80823f201c2dd
gganj123 <48715999+gganj123@users.noreply.github.com>
2025-02-06 15:14:49 +0900

    test : maptiler running

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar/navbar.jsx";
+import "./App.css";
+import Map from "./components/map/map.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Map />
+    </div>
+  );
+}
+
+export default App;
